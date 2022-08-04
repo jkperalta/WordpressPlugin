4ce4d4b9c928f66aa54bbfee01ff53f648608637
jkperalta <31081631+jkperalta@users.noreply.github.com>
2022-08-04 11:00:14 -0700

    Add files via upload

---
+++ plugin1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Insert HTML Code Sets
+* Plugin URI: C:\xampp\htdocs\mytest\wp-content\plugins\justinsplugins\plugin1.php
+* Description: This will implement adding HTML code sets according to admin specifications
+* Version: 1.0
+* Author: justinkperalta
+* Author URI: Hip Hop Stuff
+**/
+
+//does nothing for now
