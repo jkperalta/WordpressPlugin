43510db1a2a4a2436c51d396a0a6e85575f1114d
jkperalta <31081631+jkperalta@users.noreply.github.com>
2022-08-04 12:22:04 -0700

    Add files via upload

--- plugin1.php
+++ plugin1.php
@@ -8,4 +8,29 @@
 * Author URI: Hip Hop Stuff
 **/
 
-//does nothing for now
+//testing usage of add_action with custom functions and various hooks
+add_action('wp_body_open', 'insert_text');
+add_action('wp_body_open', 'get_text');
+
+function insert_text() {
+    echo "this is text";
+}
+
+function get_text() {
+
+    //extracts post content
+    $content = apply_filters('the_content', get_the_content());
+    $paragraphs = explode('</p>', $content);
+    $first_paragraph = array_shift($paragraphs).'</p>';
+
+    echo $first_paragraph;
+    $first_paragraph = array_shift($paragraphs).'</p>';
+
+    echo $first_paragraph;
+    $first_paragraph = array_shift($paragraphs).'</p>';
+
+    echo $first_paragraph;
+    echo $first_paragraph;
+    echo $first_paragraph;
+}
+
